b959a7946f5c8bae782a371a22314d4cca09a0f9
Daniel Morgan <daniel.morgan@nublue.co.uk>
2015-11-17 12:47:24 +0000

    Set up browserify and babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,54 @@
-'use strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
+
+function compile(watch) {
+  var bundler = watchify(browserify('./src/js/index.js', { debug: true }).transform(babel));
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./src'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
 
 gulp.task('sass', function() {
-    gulp.src('./src/**/*.scss')
+    gulp.src('./src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-    	.pipe(autoprefixer({
-    		browsers: ['last 2 version'],
-    		cascade: false
-		}))
+        .pipe(autoprefixer({
+           browsers: ['last 2 version'],
+           cascade: false
+        }))
         .pipe(gulp.dest('./src/'));
 });
 
-gulp.task('default', ['sass']);
-
+gulp.task('default', ['sass'], function() {
+    return compile();
+});
 gulp.task('watch', function() {
-    gulp.watch('./src/**/*.scss', ['sass'])
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+    return watch();
 });
